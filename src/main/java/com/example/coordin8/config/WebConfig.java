0f75a3914fd0a4da0006bce15c831d31c49c0657
dedunkel <skytop02@naver.com>
2025-11-22 09:12:57 +0900

    frontend server 수정

--- src/main/java/com/example/coordin8/config/WebConfig.java
+++ src/main/java/com/example/coordin8/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:3000", "http://[프론트엔드 배포 주소]") // ⭐ 프론트엔드 주소 허용
+                .allowedOrigins("http://localhost:3000", "https://front-1-iota.vercel.app/") //
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보 (쿠키 등) 허용
